b27bc0ea85d6418b7e7d94819146fc261307fb3c
erwijet <sreedoflight@gmail.com>
2020-11-10 10:33:36 -0600

    added feature to possibly redirect to auth page for autoexec in /admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,7 +2,7 @@
 // use jsoneditor (npmjs.com/package/jsoneditor)
 
 const authToken = globalThis.document.cookie.split('=')[1];
-if (!authToken) globalThis.location.replace('/auth?redirect=/admin/db?obj=' + editor.getText());
+if (!authToken) globalThis.location.replace('/auth?redirect=/admin/db?obj=' + '&mode=' + usp.get('mode') + usp.get('autoexec') == 'true' ? '&autoexec=true' : '');
 
 // Setup JSON response container
 let resContainer = document.getElementById('jsonresult');
